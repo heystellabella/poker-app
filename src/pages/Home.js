caf9ef5a429598f57ecaa34c62083b75a892cf42
Stella Ma <stella.ma96@gmail.com>
2023-02-23 20:33:48 +1100

    added content to the home and how to play pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 function Home() {
     return(
         <div>
-            <h1>HOMEPAGE</h1>
+            <div>
+                <h1>Welcome to the Home of Poker</h1>
+                <p>Stella's Poker Academy is the place to be whether you're a beginner or a poker pro.</p>
+            </div>
+            <div id="howToPlayHomePage">
+                <h3>How to Play</h3>
+                <p>If you're new to poker, we will teach you the rules of the game, hand combinations, strategies and the right poker lingo to make you look like a seasoned pro.</p>
+            </div>
+            <div id="testYourSkillsHomePage">
+                <h3>Test Your Skills</h3>
+                <p>Once you have absorbed all the knowledge we have to offer, put it in action and test your skills in our test environment to help you solidify what you've learnt.</p>
+            </div>
         </div>
     )
 }
--- src/pages/HowToPlay.js
+++ src/pages/HowToPlay.js
@@ -1,7 +1,87 @@
 function HowToPlay() {
     return (
         <div>
-            THIS IS THE HOW TO PLAY PAGE
+            <div id="introducion">
+                <h2>Learn How to Play Poker</h2>
+                <p>Learning to play poker is not as daunting as it may seem. Whether you are a beginner or a poker veteran looking for a refresher, here's what you need to know to play Texas Hold'em.</p>
+            </div>
+            <div id="objective">
+                <h2>Objective</h2>
+                <p>The objective is to win the "pot" out of a group of players. Now listen close, this is the fun part: we don't necessarily need the best hand to win.</p>
+            </div>
+            <div id="basicRules">
+                <h2>Basic Poker Rules</h2>
+            </div>
+            <div id="handRankings">
+                <h2>Hand Rankings</h2>
+                <h4 class="handCombination">Straight Flush</h4>
+                Five cards in numerical order, all of identical suits.<br/><br/>
+
+                [PICTURE]<br/><br/>
+
+                In the event of a tie: Highest rank at the top of the sequence wins.<br/><br/>
+
+                The best possible straight flush is known as a royal flush, which consists of the ace, king, queen, jack and ten of a suit. A royal flush is an unbeatable hand.<br/>
+ 
+                <h4 class="handCombination">Four of a Kind</h4>
+                Four cards of the same rank, and one side card or ‘kicker’.<br/><br/>
+
+                [PICTURE]<br/><br/>
+
+                In the event of a tie: Highest four of a kind wins. In community card games where players have the same four of a kind, the highest fifth side card ('kicker') wins.<br/>
+
+                <h4 class="handCombination">Full House</h4>
+                Three cards of the same rank, and two cards of a different, matching rank.<br/><br/>
+
+                [PICTURE]<br/><br/>
+
+                In the event of a tie: Highest three matching cards wins the pot. In community card games where players have the same three matching cards, the highest value of the two matching cards wins.<br/>
+
+                <h4 class="handCombination">Flush</h4> 
+                Five cards of the same suit.<br/><br/>
+
+                [PICTURE]<br/><br/>
+
+                In the event of a tie: The player holding the highest ranked card wins. If necessary, the second-highest, third-highest, fourth-highest, and fifth-highest cards can be used to break the tie. If all five cards are the same ranks, the pot is split. The suit itself is never used to break a tie in poker.<br/>
+
+                <h4 class="handCombination">Straight</h4>  
+                Five cards in sequence.<br/><br/>
+
+                [PICTURE]<br/><br/>
+
+                In the event of a tie: Highest ranking card at the top of the sequence wins.<br/><br/>
+
+                Note: The Ace may be used at the top or bottom of the sequence, and is the only card which can act in this manner. A,K,Q,J,T is the highest (Ace high) straight; 5,4,3,2,A is the lowest (Five high) straight.<br/>
+
+                <h4 class="handCombination">Three of a kind</h4>
+                Three cards of the same rank, and two unrelated side cards.<br/><br/>
+
+                [PICTURE]<br/><br/>
+
+                In the event of a tie: Highest ranking three of a kind wins. In community card games where players have the same three of a kind, the highest side card, and if necessary, the second-highest side card wins.<br/>
+
+                <h4 class="handCombination">Two pair</h4> 
+                Two cards of a matching rank, another two cards of a different matching rank, and one side card.<br/><br/>
+
+                [PICTURE]<br/><br/>
+
+                In the event of a tie: Highest pair wins. If players have the same highest pair, highest second pair wins. If both players have two identical pairs, highest side card wins.<br/>
+
+                <h4 class="handCombination">One pair</h4> 
+                Two cards of a matching rank, and three unrelated side cards.<br/><br/>
+
+                [PICTURE]<br/><br/>
+
+                In the event of a tie: Highest pair wins. If players have the same pair, the highest side card wins, and if necessary, the second-highest and third-highest side card can be used to break the tie.<br/>
+
+                <h4 class="handCombination">High card</h4>
+                Any hand that does not qualify under a category listed above.<br/><br/>
+
+                [PICTURE]<br/><br/>
+
+                In the event of a tie: Highest card wins, and if necessary, the second-highest, third-highest, fourth-highest and smallest card can be used to break the tie.<br/><br/>
+            </div>
+
         </div>
     )
 }
